e3ffdc6dd783b57c5e6cb5831e20a3562c98c7d0
Rodun <yo_op@naver.com>
2019-12-20 22:41:57 +0900

    Collection - List, Lambda Expression, delegate

---
+++ Collection_Test/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    void Start()
+    {
+        List<string> student_list = new List<string>();
+
+        student_list.Add("홍길동");
+        student_list.Remove("홍길동");
+        student_list.Add("박혁거세");
+        student_list.Add("선덕여왕");
+        student_list.Contains("박혁거세");
+        
+
+        List<string> new_student = student_list.FindAll(student => (student.Length >= 4));
+
+        foreach ( string _stu in new_student)
+        {
+            Debug.Log(_stu);
+        }        
+
+        //Debug.Log(student_list.FindAll(student => (student.Length >= 4)));
+
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
